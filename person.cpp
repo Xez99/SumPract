002001f84cef3fae4fde9e83b960dc6814d702f8
Xez99 <xez@MacBook-Pro-Evgenij.local>
2018-07-01 18:06:09 +0300

    v.02
    Added Person class

--- person.cpp
+++ person.cpp
@@ -1,7 +1,56 @@
 #include "person.h"
 
-Person::Person()
-{
-    //public:
+Person::Person(){
 
+}
+
+void Person::setTeam(QString team){
+    this->team = team;
+}
+
+void Person::setName(QString name){
+    this->name = name;
+}
+
+void Person::setSecondName(QString secondName){
+    this->secondName = secondName;
+}
+
+void Person::setFatherName(QString fatherName){
+    this->fatherName = fatherName;
+}
+void Person::setBirthDate(QDate birthDate){
+    this->birthDate = birthDate;
+}
+
+void Person::setPoints(unsigned points){
+    this->points = points;
+}
+
+void Person::addPoints(unsigned points){
+    this->points+=points;
+}
+
+QString Person::getTeam(){
+    return team;
+}
+
+QString Person::getName(){
+    return name;
+}
+
+QString Person::getSecondName(){
+    return secondName;
+}
+
+QString Person::getFarherName(){
+    return fatherName;
+}
+
+QDate Person::getBirthDate(){
+    return birthDate;
+}
+
+unsigned Person::getPoints(){
+    return points;
 }
--- person.h
+++ person.h
@@ -2,17 +2,34 @@
 #define PERSON_H
 
 #include "QString"
+#include "QDate"
 
 class Person
 {
 private:
+    QString team;
     QString name;
     QString secondName;
     QString fatherName;
-    unsigned short age;
+    QDate birthDate;
     unsigned points;
 public:
     Person();
+
+    void setTeam(QString team);
+    void setName(QString name);
+    void setSecondName(QString secondName);
+    void setFatherName(QString fatherName);
+    void setBirthDate(QDate birthDate);
+    void setPoints(unsigned points);
+    void addPoints(unsigned points);
+
+    QString getTeam();
+    QString getName();
+    QString getSecondName();
+    QString getFarherName();
+    QDate getBirthDate();
+    unsigned getPoints();
 };
 
 #endif // PERSON_H
